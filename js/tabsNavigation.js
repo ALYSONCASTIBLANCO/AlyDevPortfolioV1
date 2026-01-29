8cff9219b3cfa7cf06714896f099bd426066140d
acastiblanco97 <castiblanco.castaneda.alyson.val@gmail.com>
2026-01-28 22:06:35 -0500

    Experience section modified. Caminito Lab experience added and the interactions tabs with JS was implemented too. Comments are missing and pending.

--- js/tabsNavigation.js
+++ js/tabsNavigation.js
@@ -3,7 +3,7 @@
     let propTabs={
         first_heading: document.getElementById('experience_header').firstElementChild,
         first_content: document.getElementById('experience_content').firstElementChild,
-        header_styles: document.getElementById('experience_header').classList.value,
+        header_styles: document.querySelector('#experience_header li').classList.value,
         content_styles: document.getElementById('experience_content').firstElementChild.classList.value,
         header_links: document.querySelectorAll('#experience_header li a'),
         li_header: document.querySelectorAll('#experience_header li'),
@@ -13,7 +13,7 @@
     //Object with Tab Methods:
     let metTabs={
         start: function(){
-            //propTabs.first_heading.className=propTabs.header_styles+' experience-active';
+            propTabs.first_heading.className=propTabs.header_styles+' active';
             propTabs.first_content.className=propTabs.content_styles+' active';
 
             for (let i= 0; i< propTabs.header_links.length; i++) {
@@ -23,15 +23,14 @@
         },
         tabEvent: function(e){
             e.preventDefault();
-            /*
             for (let i = 0; i < propTabs.li_header.length; i++) {
                 propTabs.li_header[i].className=propTabs.header_styles;               
-            }*/
+            }
            for (let i = 0; i < propTabs.divs_content.length; i++) {
                 propTabs.divs_content[i].className=propTabs.content_styles;
             
            }
-           //this.parentElement.className = propTabs.header_styles+' experience-active';
+           this.parentElement.className = propTabs.header_styles+' active';
            propTabs.active_content=this.getAttribute('href');
            document.querySelector(propTabs.active_content).className=propTabs.content_styles+' active';
            document.querySelector(propTabs.active_content).style.opacity = 0;
